1c1461412642054f4ff24e030df9a954a4db8fcd
Muhanned Noman <Muhanned.Noman@gmail.com>
2020-09-09 11:02:30 +0300

    Chore(src): removed files and add data

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import React from 'react';
+// Import the Data.
 
 function App() {
+  const handleChange = (value) => {
+    // Do something here.
+  };
+
+  // store the Todos in a constent
+
   return (
     <div>
       <h1>Hello World!!</h1>
+      {
+        //Dispplay stuff here
+      }
     </div>
   );
 }
---
+++ src/TodoData.js
@@ -0,0 +1,24 @@
+const TodoDate = [
+  {
+    id: 1,
+    text: 'Understand HTML',
+    completed: true,
+  },
+  {
+    id: 2,
+    text: 'Understand CSS',
+    completed: true,
+  },
+  {
+    id: 3,
+    text: 'Understand JS',
+    completed: false,
+  },
+  {
+    id: 4,
+    text: 'Understand React',
+    completed: false,
+  },
+];
+
+export default TodoDate;
